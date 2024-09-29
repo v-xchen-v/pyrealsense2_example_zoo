136f58ae9242ece974c97e798f1dea638dadbbee
Xi Chen <407081305@qq.com>
2024-09-29 12:09:56 +0800

    view point cloud

---
+++ pyrealsense2_install_test.py
@@ -0,0 +1,12 @@
+# Ref: https://pypi.org/project/pyrealsense2/ Quick start
+
+import pyrealsense2 as rs
+pipe = rs.pipeline()
+profile = pipe.start()
+try:
+  for i in range(0, 100):
+    frames = pipe.wait_for_frames()
+    for f in frames:
+      print(f.profile)
+finally:
+    pipe.stop()
